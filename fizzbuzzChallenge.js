2562ae046f53dbd563c97db0c0fbcd5a39eea983
maikalangi <maikalangi@gmail.com>
2022-04-27 22:17:24 -0700

    adds js file challenge

---
+++ fizzbuzzChallenge.js
@@ -0,0 +1,36 @@
+// =====================FIZZBUZZ CHALLENGE=========================
+// Write a program that prints the numbers from 1 to 100.
+// For numbers divisible by 3, print "Fizz"
+// For numbers divisible by 5, print "Buzz"
+// For numbers divisible by both 3 && 5, print "FizzBuzz"
+
+// Hint: you will have to google the term modulo
+
+// Remember there are multiple ways to find a solution to this problem. To get your started...
+
+if (conditionOne) {
+    console.log('numbers associated with condition one');
+  } else {
+    console.log('numbers associated with some other condition');
+  }
+  
+  // or
+  
+  for(let i=0; i<=100; i++){
+    console.log(i)
+  }
+  
+  // or
+  
+  let num = 0;
+  do {
+      console.log(num);
+      num ++ 2;
+  } while (num <= 100);
+  
+  // ==========================================================================
+  // ==========================BONUS CHALLENGE=================================
+  
+  // Too easy? here's an additional challenge, create a function that returns the average of the numbers in an array.
+  
+  // No hints necessary, you got this :)
